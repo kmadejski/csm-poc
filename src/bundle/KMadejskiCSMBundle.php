3c0a1e4d3d51b17da8a29ba458b6aaa12a24fbdb
Kamil Madejski <kamil.madejski@ez.no>
2019-08-06 16:04:44 +0200

    Initial commit

---
+++ src/bundle/KMadejskiCSMBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace KMadejski\CSMBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class KMadejskiCSMBundle extends Bundle
+{
+}
---
+++ src/lib/Value/VerseUpdateData.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace KMadejski\CSM\Value;
+
+final class VerseUpdateData
+{
+    /** @var int */
+    private $contentId;
+
+    /** @var string */
+    private $fieldName;
+
+    /** @var string */
+    private $content;
+
+    /**
+     * @return int
+     */
+    public function getContentId(): int
+    {
+        return $this->contentId;
+    }
+
+    /**
+     * @param int $contentId
+     */
+    public function setContentId(int $contentId): void
+    {
+        $this->contentId = $contentId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFieldName(): string
+    {
+        return $this->fieldName;
+    }
+
+    /**
+     * @param string $fieldName
+     */
+    public function setFieldName(string $fieldName): void
+    {
+        $this->fieldName = $fieldName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+}
